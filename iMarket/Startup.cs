b57b41f8291b0ed3aedfb2369bc00cbd071b9eae
Shaheer <shaheernov123@gmail.com>
2014-10-11 21:32:14 +0500

    First Commit

---
+++ iMarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iMarket.Startup))]
+namespace iMarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
